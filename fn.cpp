81b58255d70cc3657e0f480c59af607473e71eb5
ChenLi <33598645+chenliTW@users.noreply.github.com>
2019-03-18 22:00:09 +0800

    Update fn.cpp

--- fn.cpp
+++ fn.cpp
@@ -5,6 +5,18 @@
 #include <string.h>
 #include <time.h>
 
+bool kbhit(){
+    termios term;
+    tcgetattr(0, &term);
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+    tcsetattr(0, TCSANOW, &term);
+    return byteswaiting > 0;
+}
+
 char world[SIZE+10][SIZE];
 
 void info::print_info(){
